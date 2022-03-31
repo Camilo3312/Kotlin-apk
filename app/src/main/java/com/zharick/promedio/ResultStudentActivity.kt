32b5919c8539de0bb0cb927e3c22ec5a804e1cb6
zharick <zhakmar2@gmail.com>
2022-03-31 17:18:12 -0500

    Change

--- app/src/main/java/com/zharick/promedio/ResultStudentActivity.kt
+++ app/src/main/java/com/zharick/promedio/ResultStudentActivity.kt
@@ -1,8 +1,10 @@
 package com.zharick.promedio
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 
 class ResultStudentActivity : AppCompatActivity() {
@@ -15,6 +17,7 @@ class ResultStudentActivity : AppCompatActivity() {
         val coursesS:TextView = findViewById(R.id.courses)
         val averageS:TextView = findViewById(R.id.average)
         val recuperationS:TextView = findViewById(R.id.recuperation)
+        val cardResultStudent:LinearLayout = findViewById(R.id.card_result_student)
         val pos_student = this.intent.extras?.getInt("last_student").toString().toInt()
 
         // Object student
@@ -25,13 +28,16 @@ class ResultStudentActivity : AppCompatActivity() {
 
         if(Students.stateStudent(result_prom)) {
             stateS.text = "Pasaste el periodo"
+            cardResultStudent.setBackgroundColor(Color.parseColor("#2D347E2B"))
         } else {
-            stateS.text = "Perdiste el periodo"
+
             if(Students.recuperation(result_prom)) {
                 recuperationS.text = "Usted puede recuperar"
+                cardResultStudent.setBackgroundColor(Color.parseColor("#2EFAAA34"))
             }
             else {
                 recuperationS.text = "No es posible recuperar"
+                cardResultStudent.setBackgroundColor(Color.parseColor("#2E932323"))
             }
         }
         var all_courses = ""
