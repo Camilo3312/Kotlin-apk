ba38cf57f8400c007e1a37c5c96076a81b1bc7d0
camilo3312 <ccvelasquez531@misena.edu.co>
2022-03-31 01:35:29 -0500

    Change

--- app/src/main/java/com/zharick/promedio/MainActivity.kt
+++ app/src/main/java/com/zharick/promedio/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
         val btn_show_register_students:Button = findViewById(R.id.btn_show_register_students)
         val btn_show_all_students:Button = findViewById(R.id.btn_show_all_students)
+        val btn_show_help:Button = findViewById(R.id.btn_show_help)
 
         btn_show_register_students.setOnClickListener {
             var intent = Intent(this, RegisterStudentsActivity::class.java)
@@ -21,6 +22,10 @@ class MainActivity : AppCompatActivity() {
 
         btn_show_all_students.setOnClickListener {
             var intent = Intent(this, StatisticsActivity::class.java)
+            startActivity(intent)
+        }
+        btn_show_help.setOnClickListener {
+            var intent = Intent(this, HelpActivity::class.java)
             startActivity(intent)
         }
     }
